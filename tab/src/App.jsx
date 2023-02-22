469c0ea8b5f6f608d5f73a09ae687e403e5aeb6a
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2023-02-22 16:31:18 +0100

    new changes

--- tab/src/App.jsx
+++ tab/src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+const url = 'https://course-api.com/react-tabs-project'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+  const [loading, setLoading] = React.useState(true)
+  const [info, setInfo] = React.useState([])
+  const [value, setValue] = React.useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  const fetchJobs = async () =>{
+    const response = await fetch(url)
+    const newJobs = await response.json()
+    setInfo(newJobs)
+    setLoading(false)
+  }
+  React.useEffect(()=>{
+    fetchJobs()
+  },[])
+  
+
+  if(loading){
+    return(
+      <section className="section loading">
+        <h1>please wait.........</h1>
+      </section>
+    )
+  }
+  const {company, dates, duties, title} =info[value]
+  return(
+    <section className="section">
+      <div className="title">
+      <h2>experience</h2>
+      <div className="underline"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="jobs-center">
+        <div className="btn-container"></div>
+        <article className="job-info">
+            <h3>{title}</h3>
+            <h4>{company}</h4>
+            <p className="job-date">{dates}</p>
+            {duties.map((duty,index)=>{
+              return(
+                <div key={index} className='job-desc'>
+                  <p>{duty}</p>
+                </div>
+              )
+            })}
+        </article>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+     
+    </section>
   )
 }
 
